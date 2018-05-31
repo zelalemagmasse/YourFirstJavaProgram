7b1796362a78b72c663ad36a9dd0c2351610c944
Tofik Mussa <tofahub@gmail.com>
2018-05-31 14:09:27 -0400

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello, World");
+        System.out.println("Today is July 19, 2005. I am alive");
+        System.out.println("My name is zola");
+
+    }
+}
+
+
